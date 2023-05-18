190ce1e481f42a77a05a977e05ec25d7673a4b12
Subham Sharma <subhamsharma@Subhams-MacBook-Pro.local>
2023-05-19 00:48:05 +0530

    exception handling

---
+++ ExceptionHandling/ExTryCatch2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cin>>age;
+    try{
+            if(age <= 0){
+                throw "cfd";
+            }
+            else{
+                if(age >= 18){
+                    cout<<"You are allowed to vote"<<endl;
+                }
+                else{
+                    cout<<"Can not vote now try next year"<<endl;
+                }
+            }
+    }
+    catch(int age){
+        cout<<"Wrong age"<<endl;
+    }
+    catch(...){
+        cout<<"Wrong input"<<endl;
+    }
+}
